a4a2226173a67c2b0ac809e29d8eb4ba86a69d7b
Matěj Tvrzník <matej.tvrznik@gmail.com>
2023-11-19 14:53:31 +0100

    SQL dotazy k získání jednotlivých mezd/cen potravin za období

---
+++ otazka2.sql
@@ -0,0 +1,37 @@
+-- 			MZDA 			--	
+-- první srovnané období (2006) max mzda --
+SELECT MAX(value) AS 'Nejvyšší mzda za rok 2006'
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE YEAR=2006 AND unit_value = 'Kč/month';
+
+-- druhé srovnané období (2018) max mzda -- 
+SELECT MAX(value) AS 'Nejvyšší mzda za rok 2018'
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE YEAR=2018 AND unit_value = 'Kč/month';
+
+
+--			MLÉKO			--
+-- nejvyšší cena za litr mléka v roce 2006 --
+SELECT max(value) AS 'Nejvyšší cena mléka v roce 2006', unit_value 
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE name LIKE '%Mléko%' AND YEAR = 2006;
+
+-- nejvyšší cena za litr mléka v roce 2018 --
+SELECT max(value) AS 'Nejvyšší cena mléka v roce 2018', unit_value 
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE name LIKE '%Mléko%' AND YEAR = 2018;
+
+--			CHLEBA			--
+-- nejvyšší cena za kg chleba v roce 2006 --
+SELECT max(value) AS 'Nejvyšší cena cleba v roce 2006', unit_value 
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE name LIKE '%Chleb%' AND YEAR = 2006;
+
+-- nejvyšší cena za kg chleba v roce 2018 --
+SELECT max(value) AS 'Nejvyšší cena cleba v roce 2018', unit_value 
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
+WHERE name LIKE '%Chleb%' AND YEAR = 2018;
+
+
+SELECT *
+FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
