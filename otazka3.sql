71ae25f821086800122693beb95e4d897d47c008
Matěj Tvrzník <matej.tvrznik@gmail.com>
2023-11-23 09:28:30 +0100

    zmena aliasu

--- otazka3.sql
+++ otazka3.sql
@@ -24,11 +24,11 @@ LIMIT 1;
 
 -- procentuální nárůst/pokles ceny mezi roky u jednotlivých kategorií jídla -- 
 SELECT name, value, unit_value, YEAR, 
-		lag(value, 1) OVER(ORDER BY name,YEAR) AS 'predesly rok', 
+		lag(value, 1) OVER(ORDER BY name,YEAR) AS 'predesly_rok', 
 		CASE
 			WHEN YEAR=2006 THEN NULL
 			ELSE ((value - (lag(value, 1) OVER(ORDER BY name,YEAR)))/(lag(value, 1) OVER(ORDER BY name,YEAR)))*100
-		END AS 'narust cen v %'
+		END AS 'narust_cen_v_%'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE CHAR_LENGTH(category_code) > 1 
 ORDER BY name, YEAR
