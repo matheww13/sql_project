7bd66673dd5f12ed94b9cf70df14f46fd390692b
Matěj Tvrzník <matej.tvrznik@gmail.com>
2023-12-16 10:48:13 +0100

    Změna názvů slouců z aj do cz

--- otazka1.sql
+++ otazka1.sql
@@ -1,10 +1,10 @@
 -- odvětví kde došlo k poklesu příjmů -- 
 SELECT
-	name,
-	value,
-	YEAR,
-	Last_year_pay,
-	up_or_down
+	name AS 'Odvětví',
+	value AS 'Mzda',
+	YEAR AS 'Rok',
+	Last_year_pay AS 'Lonska_mzda',
+	up_or_down AS 'Roste/klesá'
 FROM ( 
 	SELECT
 	name,
@@ -28,17 +28,17 @@ ORDER BY name
 -- query pro všechny roky a odvětví --
 
 SELECT
-	name,
-	value,
-	YEAR,
-	LAG (round(avg(value), 0)) OVER ( ORDER BY name, `year` ) AS 'last_year_avg_pay',
+	name AS 'Odvětví',
+	value AS 'Mzda',
+	YEAR AS 'Rok',
+	LAG (round(avg(value), 0)) OVER ( ORDER BY name, `year` ) AS 'Lonska_mzda',
 	CASE
 		WHEN  year = 2006 THEN NULL
 		WHEN round(avg(value), 0) > LAG (round(avg(value), 0)) OVER ( 
   		ORDER BY name, year 
 		) THEN TRUE 
 	ELSE FALSE
-	END AS 'up_or_down'
+	END AS 'Roste/klesa'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE unit_value = 'Kč/month'
 GROUP BY name, `year` 
@@ -46,7 +46,7 @@ ORDER BY name;
 
 
 -- počet odvětví --
-SELECT COUNT(DISTINCT name) AS 'sum_every_industry'
+SELECT COUNT(DISTINCT name) AS 'Součet odvětví'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE unit_value  = 'Kč/month';
  
