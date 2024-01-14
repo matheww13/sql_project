b7329c8f1fec2ffbe7ab431d2af0e0b5ce7a6beb
Matěj Tvrzník <matej.tvrznik@gmail.com>
2024-01-14 12:20:14 +0100

    Názvy sloupců do ENG

--- otazka2.sql
+++ otazka2.sql
@@ -1,20 +1,20 @@
 -- kolik litrů mléka je možné koupit s nejvyšší mzdou v roce 2006 -- 
-SELECT	tmtpspf.value AS 'Cena mléka v roce 2006',
-		tmtpspf.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value) AS 'Nejvyšší mzda za rok 2006',
-		tmtpspf2.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value)/tmtpspf.value AS 'Kolik litrů je možné koupit'
+SELECT	tmtpspf.value AS 'Milk_price_2006',
+		tmtpspf.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value) AS 'Highest_salary_2006',
+		tmtpspf2.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value)/tmtpspf.value AS 'How_many_liters_can_be_bought'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 JOIN t_matej_tvrznik_project_SQL_primary_final tmtpspf2 ON tmtpspf.`year` = tmtpspf2.`year` 
 WHERE tmtpspf.name LIKE '%Mleko%' 
 	  AND tmtpspf.YEAR = 2006;
 		 
 -- kolik litrů mléka je možné koupit s nejvyšší mzdou v roce 2018 -- 
-SELECT	tmtpspf.value AS 'Cena mléka v roce 2018',
-		tmtpspf.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value) AS 'Nejvyšší mzda za rok 2018',
-		tmtpspf2.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value)/tmtpspf.value AS 'Kolik litrů je možné koupit'
+SELECT	tmtpspf.value AS 'Milk_price_2018',
+		tmtpspf.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value) AS 'Highest_salary_2018',
+		tmtpspf2.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value)/tmtpspf.value AS 'How_many_liters_can_be_bought'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 JOIN t_matej_tvrznik_project_SQL_primary_final tmtpspf2 ON tmtpspf.`year` = tmtpspf2.`year` 
 WHERE tmtpspf.name LIKE '%Mleko%' 
@@ -22,22 +22,22 @@ WHERE tmtpspf.name LIKE '%Mleko%'
 		 
 		
 -- kolik kg chleba je možné koupit s nejvyšší mzdou v roce 2006 -- 
-SELECT	tmtpspf.value AS 'Cena chleba v roce 2006',
-		tmtpspf.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value) AS 'Nejvyšší mzda za rok 2006',
-		tmtpspf2.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value)/tmtpspf.value AS 'Kolik kg je možné koupit'
+SELECT	tmtpspf.value AS 'Bread_price_2006',
+		tmtpspf.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value) AS 'Highest_salary_2006',
+		tmtpspf2.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value)/tmtpspf.value AS 'How_many_kgs_can_be_bought'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 JOIN t_matej_tvrznik_project_SQL_primary_final tmtpspf2 ON tmtpspf.`year` = tmtpspf2.`year` 
 WHERE tmtpspf.name LIKE '%Chleb%' 
 	  AND tmtpspf.YEAR = 2006;
 
 -- kolik kg chleba je možné koupit s nejvyšší mzdou v roce 2018 -- 
-SELECT	tmtpspf.value AS 'Cena chleba v roce 2018',
-		tmtpspf.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value) AS 'Nejvyšší mzda za rok 2018',
-		tmtpspf2.unit_value AS 'měna/jednotky',
-		MAX(tmtpspf2.value)/tmtpspf.value AS 'Kolik kg je možné koupit'
+SELECT	tmtpspf.value AS 'Bread_price_2018',
+		tmtpspf.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value) AS 'Highest_salary_2018',
+		tmtpspf2.unit_value AS 'currency/unit',
+		MAX(tmtpspf2.value)/tmtpspf.value AS 'How_many_kgs_can_be_bought'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 JOIN t_matej_tvrznik_project_SQL_primary_final tmtpspf2 ON tmtpspf.`year` = tmtpspf2.`year` 
 WHERE tmtpspf.name LIKE '%Chleb%' 
@@ -49,34 +49,34 @@ WHERE tmtpspf.name LIKE '%Chleb%'
 -- Jednotlivé položky a jejich cena/mzdy a jejich výška pro kontrolu --
 -- 			MZDA 			--	
 -- první srovnané období (2006) max mzda --
-SELECT MAX(value) AS 'Nejvyšší mzda za rok 2006'
+SELECT MAX(value) AS 'Highest_salary_2006'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE YEAR=2006 AND unit_value = 'Kč/month';
 
 -- druhé srovnané období (2018) max mzda -- 
-SELECT MAX(value) AS 'Nejvyšší mzda za rok 2018'
+SELECT MAX(value) AS 'Highest_salary_2018'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE YEAR=2018 AND unit_value = 'Kč/month';
 
 
 --			MLÉKO			--
 -- cena za litr mléka v roce 2006 --
-SELECT value AS 'Nejvyšší cena mléka v roce 2006', unit_value AS 'měna/jednotky'
+SELECT value AS 'Highest_milk_price_2006', unit_value AS 'currency/unit'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE name LIKE '%Mléko%' AND YEAR = 2006;
 
 -- cena za litr mléka v roce 2018 --
-SELECT value AS 'Nejvyšší cena mléka v roce 2018', unit_value AS 'měna/jednotky'
+SELECT value AS 'Highest_milk_price_2018', unit_value AS 'currency/unit'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE name LIKE '%Mléko%' AND YEAR = 2018;
 
 --			CHLEBA			--
 -- cena za kg chleba v roce 2006 --
-SELECT value AS 'Nejvyšší cena cleba v roce 2006', unit_value AS 'měna/jednotky'
+SELECT value AS 'Highest_bread_price_2006', unit_value AS 'currency/unit'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE name LIKE '%Chleb%' AND YEAR = 2006;
 
 -- cena za kg chleba v roce 2018 --
-SELECT value AS 'Nejvyšší cena cleba v roce 2018', unit_value AS 'měna/jednotky'
+SELECT value AS 'Highest_bread_price_2018', unit_value AS 'currency/unit'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE name LIKE '%Chleb%' AND YEAR = 2018;
