545d37b2a481493815f3669b45e01e208e985de8
Matěj Tvrzník <matej.tvrznik@gmail.com>
2024-01-14 12:24:49 +0100

    Názvy sloupců do ENG

--- otazka3.sql
+++ otazka3.sql
@@ -1,9 +1,9 @@
 -- sum procentuálního nárůstu/poklesu ceny a získání odpovědi na otázku 3 --
 SELECT
-	name AS 'Název',
-	value AS 'Hodnota',
-	unit_value AS 'měna/jednotky',
-	avg(narust_cen_percent) AS avg_narust_cen_procenta
+	name AS 'Name',
+	value AS 'Value',
+	unit_value AS '',
+	avg(price_increase_percent) AS avg_price_increase_percent
 FROM 
 	(SELECT 
 		name,
@@ -13,25 +13,25 @@ FROM
 		CASE
 			WHEN YEAR = 2006 THEN NULL
 			ELSE ((value - (lag(value, 1) OVER(ORDER BY name,YEAR))) / (lag(value, 1) OVER(ORDER BY name,YEAR))) * 100
-		END AS narust_cen_percent
+		END AS price_increase_percent
 	FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 	WHERE CHAR_LENGTH(category_code) > 1 
 	ORDER BY name, YEAR) AS sub
 GROUP BY name
-ORDER BY avg_narust_cen_procenta
+ORDER BY avg_price_increase_percent
 LIMIT 1;
 
 
 -- procentuální nárůst/pokles ceny mezi roky u jednotlivých kategorií jídla -- 
-SELECT name AS 'Název',
-		value AS 'Hodnota',
-		unit_value AS 'měna/jednotky',
-		YEAR AS 'Rok', 
-		lag(value, 1) OVER(ORDER BY name,YEAR) AS 'predesly_rok', 
+SELECT name AS 'Name',
+		value AS 'Value',
+		unit_value AS 'currency/unit',
+		YEAR AS 'Year', 
+		lag(value, 1) OVER(ORDER BY name,YEAR) AS 'Last_year', 
 		CASE
 			WHEN YEAR=2006 THEN NULL
 			ELSE ((value - (lag(value, 1) OVER(ORDER BY name,YEAR)))/(lag(value, 1) OVER(ORDER BY name,YEAR)))*100
-		END AS 'narust_cen_v_%'
+		END AS 'price_increase_percent'
 FROM t_matej_tvrznik_project_SQL_primary_final tmtpspf 
 WHERE CHAR_LENGTH(category_code) > 1 
 ORDER BY name, YEAR
